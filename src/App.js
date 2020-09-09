83d879a5abb70b8ed6cd6a1388abbcef73a7b2d7
emran899dev <mdemrannazir9999@gmail.com>
2020-09-09 03:47:10 -0600

    logged in user in state display logged in user

--- src/App.js
+++ src/App.js
@@ -1,24 +1,54 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+// Firebase App (the core Firebase SDK) is always required and must be listed first
+import * as firebase from "firebase/app";
+// Add the Firebase products that you want to use
+import "firebase/auth";
+// firebaseConfig
+import firebaseConfig from './firebase.config';
+
+
+firebase.initializeApp(firebaseConfig)
 
 function App() {
+  const [user, setUser] = useState({
+    isSignedIn: false,
+    name: '',
+    email: '',
+    photo: ''
+  })
+
+  const provider = new firebase.auth.GoogleAuthProvider();
+  const handelSignIn = () =>{
+    firebase.auth().signInWithPopup(provider)
+    .then(res => {
+      const {displayName,email,photoURL} = res.user
+      const signedInUser = {
+        isSignedIn: true,
+        name: displayName,
+        email: email,
+        photo: photoURL
+      }
+    setUser(signedInUser)
+
+      // console.log(displayName,email,photoURL);
+
+    })
+    .catch(err => {
+      console.log(err);
+      console.log(err.message)
+    })
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <button onClick={handelSignIn}>Sign in</button>
+     {
+       user.isSignedIn && <div>
+         <p>Welcome, {user.name}</p>
+         <p>Email, {user.email}</p>
+         <img src={user.photo} alt=""/>
+       </div>
+     }
     </div>
   );
 }
---
+++ src/firebase.config.js
@@ -0,0 +1,12 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyDwtFr8ON-1s31zhgCR3zRdQLDGpn0sMQ4",
+  authDomain: "ema-johns.firebaseapp.com",
+  databaseURL: "https://ema-johns.firebaseio.com",
+  projectId: "ema-johns",
+  storageBucket: "ema-johns.appspot.com",
+  messagingSenderId: "430793452125",
+  appId: "1:430793452125:web:cc0e9515e3ca60634b3bb4"
+};
+
+
+export default firebaseConfig;
